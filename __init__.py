b2203e27224c06c167bf9a3341d3695087b379c7
Adam Hassan <114709584+adamhassan66@users.noreply.github.com>
2023-01-11 20:31:29 -0700

    first commit

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from flask_app import flask
+app = Flask (__name__)
+app.secret_key = 'no secrets on github'
+DATABASE = 'users_schema'
---
+++ flask_app/controllers/controller.py
@@ -0,0 +1,4 @@
+from flask bcrypt import bcrypt
+bcrypt = Bycrypt(app)
+
+@app.route
